baa0a4d1be8b82835805ca70e7777020c0987316
Daniel Granados <20200008@ricaldone.edu.sv>
2024-06-24 12:14:29 -0600

    ViewHolderPacientes listo

--- app/src/main/java/pacientesHelper/ViewHolderPacientes.kt
+++ app/src/main/java/pacientesHelper/ViewHolderPacientes.kt
@@ -1,4 +1,14 @@
 package pacientesHelper
 
-class ViewHolderPacientes {
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import daniel.granados.myapplication.R
+
+class ViewHolderPacientes(view: View) {
+    val lblApellidoPaciente: TextView = view.findViewById(R.id.lblApellidosPaciente)
+    val lblHabitacion: TextView = view.findViewById(R.id.lblHabitacionPaciente)
+    val lblHoraControl: TextView = view.findViewById(R.id.lblHorarioPaciente)
+
+    val imgBorrar: ImageView = view.findViewById(R.id.imgBorrar)
 }
